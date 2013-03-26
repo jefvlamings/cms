35302484d1082ec595106ae4e7eb3e5868002ed0
Jef Vlamings <vlamingsjef@gmail.com>
2013-03-26 21:13:35 +0100

    Admin app added Routing fixed for admin app Admin app now has its own template and static files Bootstrap integration (minimal setup)

--- admin/urls.py
+++ admin/urls.py
@@ -1,11 +1,8 @@
 from django.conf.urls import patterns, url
-from pages import views
+import views
 
 # Routing
 urlpatterns = patterns('',
-
-    # Any string should redirect to 'detail'
-    url(r'(?P<path>.+)$', views.detail, name='detail'),
 
     # An empty string ^$ should redirect to 'index'
     url(r'^$', views.index, name='index'),
--- cms/urls.py
+++ cms/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
+    url(r'^admin', include('admin.urls')),
     url(r'^', include('pages.urls')),
 )
